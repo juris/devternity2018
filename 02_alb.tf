1931ffa8d77a4e74475ed41f63a98fe70c85e611
Juris <juris@zee.lv>
2018-11-28 16:04:31 +0200

    Minor alb modifications (healthchecks, timeouts, stuff).

--- 02_alb.tf
+++ 02_alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "default_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.thumbor_sg.id}"]
-  subnets            = ["${aws_default_subnet.default_az1a.id}","${aws_default_subnet.default_az1b.id}"]
+  subnets            = ["${aws_default_subnet.default_az1a.id}", "${aws_default_subnet.default_az1b.id}"]
 
   tags = "${merge(
     local.common_tags,
@@ -25,20 +25,22 @@ resource "aws_lb_listener" "thumbor_listener" {
 }
 
 resource "aws_lb_target_group" "thumbor_tg" {
-  name     = "thumbor-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${aws_default_vpc.default_vpc.id}"
+  name                 = "thumbor-tg"
+  port                 = "80"
+  protocol             = "HTTP"
+  vpc_id               = "${aws_default_vpc.default_vpc.id}"
+  deregistration_delay = "10"
+
   health_check {
-      interval = "10"
-      healthy_threshold = "2"
-      unhealthy_threshold = "2"
-      path = "/healthcheck"
+    interval            = "10"
+    healthy_threshold   = "2"
+    unhealthy_threshold = "2"
+    path                = "/healthcheck"
   }
 }
 
-resource "aws_lb_target_group_attachment" "thumbor_tg_attachment" {
-  target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
-  target_id        = "${aws_instance.thumbor.id}"
-  port             = 80
-}
+# resource "aws_lb_target_group_attachment" "thumbor_tg_attachment" {
+#   target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
+#   target_id        = "${aws_instance.thumbor.id}"
+#   port             = "80"
+# }
