0644cdb6e21b8516af6bf15138ee5d031154572b
Juris <juris@zee.lv>
2018-11-29 12:55:17 +0200

    Comment all tf manifests for step-by-step execution.

--- 02_alb.tf
+++ 02_alb.tf
@@ -1,46 +1,46 @@
-resource "aws_lb" "default_alb" {
-  name               = "${local.common_tags["project"]}-${local.common_tags["env"]}-alb01"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.thumbor_sg.id}"]
-  subnets            = ["${aws_default_subnet.default_az1a.id}", "${aws_default_subnet.default_az1b.id}"]
+# resource "aws_lb" "default_alb" {
+#   name               = "${local.common_tags["project"]}-${local.common_tags["env"]}-alb01"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = ["${aws_security_group.thumbor_sg.id}"]
+#   subnets            = ["${aws_default_subnet.default_az1a.id}", "${aws_default_subnet.default_az1b.id}"]
 
-  tags = "${merge(
-    local.common_tags,
-    map(
-      "Name", "${local.common_tags["project"]}-${local.common_tags["env"]}-alb01"
-    )
-  )}"
-}
-
-resource "aws_lb_listener" "thumbor_listener" {
-  load_balancer_arn = "${aws_lb.default_alb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
-  }
-}
-
-resource "aws_lb_target_group" "thumbor_tg" {
-  name                 = "thumbor-tg"
-  port                 = "80"
-  protocol             = "HTTP"
-  vpc_id               = "${aws_default_vpc.default_vpc.id}"
-  deregistration_delay = "10"
-
-  health_check {
-    interval            = "10"
-    healthy_threshold   = "2"
-    unhealthy_threshold = "2"
-    path                = "/healthcheck"
-  }
-}
-
-# resource "aws_lb_target_group_attachment" "thumbor_tg_attachment" {
-#   target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
-#   target_id        = "${aws_instance.thumbor.id}"
-#   port             = "80"
+#   tags = "${merge(
+#     local.common_tags,
+#     map(
+#       "Name", "${local.common_tags["project"]}-${local.common_tags["env"]}-alb01"
+#     )
+#   )}"
 # }
+
+# resource "aws_lb_listener" "thumbor_listener" {
+#   load_balancer_arn = "${aws_lb.default_alb.arn}"
+#   port              = "80"
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
+#   }
+# }
+
+# resource "aws_lb_target_group" "thumbor_tg" {
+#   name                 = "thumbor-tg"
+#   port                 = "80"
+#   protocol             = "HTTP"
+#   vpc_id               = "${aws_default_vpc.default_vpc.id}"
+#   deregistration_delay = "10"
+
+#   health_check {
+#     interval            = "10"
+#     healthy_threshold   = "2"
+#     unhealthy_threshold = "2"
+#     path                = "/healthcheck"
+#   }
+# }
+
+# # resource "aws_lb_target_group_attachment" "thumbor_tg_attachment" {
+# #   target_group_arn = "${aws_lb_target_group.thumbor_tg.arn}"
+# #   target_id        = "${aws_instance.thumbor.id}"
+# #   port             = "80"
+# # }
--- 03_lt_asg.tf
+++ 03_lt_asg.tf
@@ -1,64 +1,64 @@
-resource "aws_launch_template" "thumbor_lt" {
-  name = "thumbor_lt"
+# resource "aws_launch_template" "thumbor_lt" {
+#   name = "thumbor_lt"
 
-  image_id               = "ami-0bdf93799014acdc4"
-  instance_type          = "t2.micro"
-  key_name               = "${aws_key_pair.default_ssh_key.id}"
-  vpc_security_group_ids = ["${aws_security_group.thumbor_sg.id}"]
-  user_data              = "${base64encode(file("tf-data/cloud-init-asg"))}"
+#   image_id               = "ami-0bdf93799014acdc4"
+#   instance_type          = "t2.micro"
+#   key_name               = "${aws_key_pair.default_ssh_key.id}"
+#   vpc_security_group_ids = ["${aws_security_group.thumbor_sg.id}"]
+#   user_data              = "${base64encode(file("tf-data/cloud-init-asg"))}"
 
-  monitoring {
-    enabled = true
-  }
+#   monitoring {
+#     enabled = true
+#   }
 
-  block_device_mappings {
-    device_name = "/dev/sda1"
+#   block_device_mappings {
+#     device_name = "/dev/sda1"
 
-    ebs {
-      volume_size = 8
-    }
-  }
+#     ebs {
+#       volume_size = 8
+#     }
+#   }
 
-  tag_specifications {
-    resource_type = "instance"
+#   tag_specifications {
+#     resource_type = "instance"
 
-    tags = "${merge(
-    local.common_tags,
-    map(
-      "Name", "${local.common_tags["project"]}-${local.common_tags["env"]}-asg-thumbor01"
-    )
-  )}"
-  }
-}
+#     tags = "${merge(
+#     local.common_tags,
+#     map(
+#       "Name", "${local.common_tags["project"]}-${local.common_tags["env"]}-asg-thumbor01"
+#     )
+#   )}"
+#   }
+# }
 
-resource "aws_autoscaling_group" "thumbor_asg" {
-  name               = "thumbor_asg"
-  availability_zones = ["eu-central-1a", "eu-central-1b"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-  default_cooldown   = 60
-  health_check_type  = "ELB"
-  target_group_arns  = ["${aws_lb_target_group.thumbor_tg.arn}"]
+# resource "aws_autoscaling_group" "thumbor_asg" {
+#   name               = "thumbor_asg"
+#   availability_zones = ["eu-central-1a", "eu-central-1b"]
+#   desired_capacity   = 1
+#   max_size           = 2
+#   min_size           = 1
+#   default_cooldown   = 60
+#   health_check_type  = "ELB"
+#   target_group_arns  = ["${aws_lb_target_group.thumbor_tg.arn}"]
 
-  launch_template = {
-    id      = "${aws_launch_template.thumbor_lt.id}"
-    version = "$$Latest"
-  }
-}
+#   launch_template = {
+#     id      = "${aws_launch_template.thumbor_lt.id}"
+#     version = "$$Latest"
+#   }
+# }
 
-resource "aws_autoscaling_policy" "high_cpu_scaleup" {
-  name                      = "high_cpu_scaleup"
-  adjustment_type           = "ChangeInCapacity"
-  autoscaling_group_name    = "${aws_autoscaling_group.thumbor_asg.name}"
-  policy_type               = "TargetTrackingScaling"
-  estimated_instance_warmup = 10
+# resource "aws_autoscaling_policy" "high_cpu_scaleup" {
+#   name                      = "high_cpu_scaleup"
+#   adjustment_type           = "ChangeInCapacity"
+#   autoscaling_group_name    = "${aws_autoscaling_group.thumbor_asg.name}"
+#   policy_type               = "TargetTrackingScaling"
+#   estimated_instance_warmup = 10
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
 
-    target_value = 50.0
-  }
-}
+#     target_value = 50.0
+#   }
+# }
